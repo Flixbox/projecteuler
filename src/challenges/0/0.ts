fcffb23a5810d891c18091834cdf0d9256b4dbb4
Flixbox <felix@tietjen.it>
2025-12-18 14:40:58 +0100

    first simplistic attempt

--- src/challenges/0/0.ts
+++ src/challenges/0/0.ts
@@ -1,10 +1,16 @@
 const SQUARE_NUMBERS_TO_FIND = 274000
 let amountOfFoundSquareNumbers = 0
 let sumOfSquareNumbers = 0
+let currentIteratorNumber = 1
 
-while(amountOfFoundSquareNumbers < SQUARE_NUMBERS_TO_FIND) {
+while (amountOfFoundSquareNumbers < SQUARE_NUMBERS_TO_FIND) {
+  const root = Math.sqrt(currentIteratorNumber)
+  const isSquareNumber = Number.isInteger(root)
+  if (isSquareNumber) {
     amountOfFoundSquareNumbers++
-    sumOfSquareNumbers++
+    sumOfSquareNumbers += currentIteratorNumber
+  }
+  currentIteratorNumber++
 }
 
-console.info("sumOfSquareNumbers: ", sumOfSquareNumbers)
+console.info('sumOfSquareNumbers: ', sumOfSquareNumbers)
