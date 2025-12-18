4196475aa9b10de5696c31d49a6401962c00ff45
Flixbox <felix@tietjen.it>
2025-12-18 14:50:01 +0100

    improve performance

--- src/challenges/0/0.ts
+++ src/challenges/0/0.ts
@@ -19,18 +19,10 @@
  */
 
 const SQUARE_NUMBERS_TO_FIND = 274000
-let amountOfFoundSquareNumbers = 0
 let sumOfSquareNumbers = 0
-let currentIteratorNumber = 1
 
-while (amountOfFoundSquareNumbers < SQUARE_NUMBERS_TO_FIND) {
-  const root = Math.sqrt(currentIteratorNumber)
-  const isSquareNumber = Number.isInteger(root)
-  if (isSquareNumber) {
-    amountOfFoundSquareNumbers++
-    sumOfSquareNumbers += currentIteratorNumber
-  }
-  currentIteratorNumber++
+for (let i = 1; i <= SQUARE_NUMBERS_TO_FIND; i++) {
+  sumOfSquareNumbers += i * i
 }
 
 console.info('sumOfSquareNumbers: ', sumOfSquareNumbers)
