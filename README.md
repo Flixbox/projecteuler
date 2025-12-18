d0ff876b2bae17f759ee0fe29aae8811ea641eb5
Flixbox <felix@tietjen.it>
2025-12-18 14:33:56 +0100

    challenge 0 initial setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # projecteuler
 My personal solutions to Project Euler challenges. https://projecteuler.net/about
+
+# Setup
+
+- Install bun
+- `bun i`
+- `bun run .\src\challenges\0\0` (replace with desired challenge)
--- src/challenges/0/0.ts
+++ src/challenges/0/0.ts
@@ -1,1 +1,10 @@
-const squareNumberLimit = 
+const SQUARE_NUMBERS_TO_FIND = 274000
+let amountOfFoundSquareNumbers = 0
+let sumOfSquareNumbers = 0
+
+while(amountOfFoundSquareNumbers < SQUARE_NUMBERS_TO_FIND) {
+    amountOfFoundSquareNumbers++
+    sumOfSquareNumbers++
+}
+
+console.info("sumOfSquareNumbers: ", sumOfSquareNumbers)
